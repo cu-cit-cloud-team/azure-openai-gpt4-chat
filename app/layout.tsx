2c350d125bc40b8eff3c2c0f45aa48bc8ccd8a8d
Michael Sprague <mikesprague@gmail.com>
2023-10-04 19:43:40 -0400

    refactor(layout): remove favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,6 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="night">
       <body>
-        <link rel="icon" href="/azure-openai.png" />
         <div className="page-wrapper">{children}</div>
       </body>
     </html>
