9356387f98bbc4d6cbbc9038eb5b5e383d123ab6
Michael Sprague <mikesprague@gmail.com>
2023-10-04 20:48:47 -0400

    refactor(page): use ChatBubble component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
 'use client';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useChat } from 'ai/react';
-import dayjs from 'dayjs';
 import { nanoid } from 'nanoid';
 import { useEffect, useRef } from 'react';
-import Markdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import {
-  materialDark,
-  nightOwl,
-} from 'react-syntax-highlighter/dist/esm/styles/prism';
 import useLocalStorageState from 'use-local-storage-state';
 
 import pkg from '../package.json';
 
+import { ChatBubble } from './components/ChatBubble';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 
@@ -94,67 +85,14 @@ export default function Chat() {
             ? messages.map((m, idx) => {
                 const isUser = m.role === 'user';
                 return (
-                  <div
-                    key={m.id}
-                    ref={idx === messages.length - 1 ? lastMessageRef : null}
-                    className={`chat ${isUser ? 'chat-start' : 'chat-end'}`}
-                  >
-                    <div className="chat-image avatar">
-                      <div className="w-12 p-2 rounded bg-base-300">
-                        <img
-                          src={`/${isUser ? 'icon-user.png' : 'icon-bot.png'}`}
-                          alt=""
-                          className="invert"
-                        />
-                      </div>
-                    </div>
-                    <div className="prose chat-bubble">
-                      <Markdown
-                        children={m.content}
-                        components={{
-                          code(props) {
-                            const { children, className, node, ...rest } =
-                              props;
-                            const match = /language-(\w+)/.exec(
-                              className || ''
-                            );
-                            return match ? (
-                              <SyntaxHighlighter
-                                {...rest}
-                                children={String(children).replace(/\n$/, '')}
-                                style={nightOwl}
-                                language={match[1]}
-                                PreTag="div"
-                              />
-                            ) : (
-                              <code {...rest} className={className}>
-                                {children}
-                              </code>
-                            );
-                          },
-                        }}
-                      />
-                    </div>
-                    <div className="chat-footer">
-                      {isUser || idx !== messages.length - 1 ? (
-                        <time className="text-xs opacity-50">
-                          {dayjs(m.createdAt).format('h:mm A')}
-                        </time>
-                      ) : null}
-                      {isLoading && !isUser && idx === messages.length - 1 ? (
-                        <FontAwesomeIcon
-                          icon={faSpinner}
-                          spinPulse
-                          fixedWidth
-                        />
-                      ) : null}
-                      {idx === messages.length - 1 && !isLoading ? (
-                        <time className="text-xs opacity-50">
-                          {dayjs(m.createdAt).format('h:mm A')}
-                        </time>
-                      ) : null}
-                    </div>
-                  </div>
+                  <ChatBubble
+                    message={m}
+                    index={idx}
+                    isUser={isUser}
+                    isLoading={isLoading}
+                    lastMessageRef={lastMessageRef}
+                    totalMessages={messages.length}
+                  />
                 );
               })
             : null}
