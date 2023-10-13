33ef6a0ad1911a72122ad707c71e19f2b54ab1fd
Michael Sprague <mikesprague@gmail.com>
2023-10-13 10:09:21 -0400

    feat(utils): add mardownToText helper

---
+++ app/utils/markdownToText.ts
@@ -0,0 +1,45 @@
+import { marked } from 'marked';
+
+const block = (text: string) => `${text}\n\n`;
+const escapeBlock = (text: string) => `${encodeURIComponent(text)}\n\n`;
+const line = (text: string) => `${text}\n`;
+const inline = (text: string) => text;
+const newline = () => '\n';
+const empty = () => '';
+
+const TextRenderer: marked.Renderer = {
+  // block elements
+  code: escapeBlock,
+  blockquote: block,
+  html: empty,
+  heading: block,
+  hr: newline,
+  list: (text) => block(text.trim()),
+  listitem: line,
+  checkbox: empty,
+  paragraph: block,
+  table: (header, body) => line(header + body),
+  tablerow: (text) => line(text.trim()),
+  tablecell: (text) => `${text} `,
+  // inline elements
+  strong: inline,
+  em: inline,
+  codespan: inline,
+  br: newline,
+  del: inline,
+  link: (_0, _1, text) => text,
+  image: (_0, _1, text) => text,
+  text: inline,
+  // etc.
+  options: {},
+};
+
+export const markdownToText = (
+  markdown: string,
+  options?: marked.MarkedOptions
+): string => {
+  const unmarked = marked(markdown, { ...options, renderer: TextRenderer });
+  const unescaped = decodeURIComponent(unmarked);
+  const trimmed = unescaped.trim();
+  return trimmed;
+};
