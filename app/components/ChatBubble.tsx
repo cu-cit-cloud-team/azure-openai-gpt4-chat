4cdba84e5a7039169e52c53c89b3e0442a06f51e
Michael Sprague <mikesprague@gmail.com>
2023-10-15 12:30:17 -0400

    feat(ChatBubble): use relative time for chat timestamps

--- app/components/ChatBubble.tsx
+++ app/components/ChatBubble.tsx
@@ -2,8 +2,9 @@ import { faRobot, faSpinner, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import dayjs from 'dayjs';
 import isToday from 'dayjs/plugin/isToday';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Markdown from 'react-markdown';
 import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { nightOwl } from 'react-syntax-highlighter/dist/esm/styles/prism';
@@ -13,6 +14,7 @@ import { markdownToText } from '../utils/markdownToText.ts';
 import { CopyToClipboard } from './CopyToClipboard.tsx';
 
 dayjs.extend(isToday);
+dayjs.extend(relativeTime);
 
 export const ChatBubble = ({
   message,
@@ -21,85 +23,114 @@ export const ChatBubble = ({
   isLoading,
   totalMessages,
   userMeta,
-}) => (
-  <div
-    key={message.id}
-    className={`chat ${isUser ? 'chat-start' : 'chat-end'}`}
-  >
-    <div className="chat-header">
-      {isUser || index !== totalMessages ? (
-        <time className="text-xs opacity-50">
-          {dayjs(message.createdAt).isToday()
-            ? dayjs(message.createdAt).format('hh:mm A')
-            : dayjs(message.createdAt).format('ddd MMM DD [at] h:mm A')}
-        </time>
-      ) : null}
-      {index === totalMessages && !isLoading ? (
-        <time className="text-xs opacity-50">
-          {dayjs(message.createdAt).isToday()
-            ? dayjs(message.createdAt).format('hh:mm A')
-            : dayjs(message.createdAt).format('ddd MMM DD [at] h:mm A')}
-        </time>
-      ) : null}
-    </div>
-    <div className="chat-image avatar">
+}) => {
+  const [lastUpdatedString, setLastUpdatedString] = useState('');
+
+  useEffect(() => {
+    if (!message) {
+      return;
+    }
+
+    const updateString = () => {
+      setLastUpdatedString(dayjs(dayjs(message.createdAt)).from());
+    };
+    const clockInterval = setInterval(updateString, 1000);
+
+    updateString();
+
+    return () => clearInterval(clockInterval);
+  }, []);
+
+  return (
+    <div
+      key={message.id}
+      className={`chat ${isUser ? 'chat-start' : 'chat-end'}`}
+    >
+      <div className="chat-header">
+        {isUser || index !== totalMessages ? (
+          <time
+            className="text-xs opacity-50 tooltip"
+            data-tip={
+              dayjs(message.createdAt).isToday()
+                ? dayjs(message.createdAt).format('hh:mm a')
+                : dayjs(message.createdAt).format('ddd MMM DD YYYY [at] h:mm a')
+            }
+          >
+            {lastUpdatedString}
+          </time>
+        ) : null}
+        {index === totalMessages && !isLoading ? (
+          <time
+            className="text-xs opacity-50 tooltip"
+            data-tip={
+              dayjs(message.createdAt).isToday()
+                ? dayjs(message.createdAt).format('hh:mm a')
+                : dayjs(message.createdAt).format('ddd MMM DD YYYY [at] h:mm a')
+            }
+          >
+            {lastUpdatedString}
+          </time>
+        ) : null}
+      </div>
+      <div className="chat-image avatar">
+        <div
+          className={`w-12 pt-2 p-1 rounded text-neutral-content ${
+            isUser ? 'bg-primary' : 'bg-neutral'
+          }`}
+        >
+          <FontAwesomeIcon
+            className="chat-avatar-icon"
+            size="2x"
+            icon={isUser ? faUser : faRobot}
+            fixedWidth
+          />
+        </div>
+      </div>
       <div
-        className={`w-12 pt-2 p-1 rounded text-neutral-content ${
-          isUser ? 'bg-primary' : 'bg-neutral'
+        className={`prose relative chat-bubble${
+          isUser ? ' chat-bubble-primary' : ' bot'
         }`}
       >
-        <FontAwesomeIcon
-          className="chat-avatar-icon"
-          size="2x"
-          icon={isUser ? faUser : faRobot}
-          fixedWidth
+        {isUser
+          ? null
+          : (!isLoading || index !== totalMessages) && (
+              <CopyToClipboard textToCopy={markdownToText(message.content)} />
+            )}
+        <Markdown
+          children={message.content}
+          components={{
+            code(props) {
+              // biome-ignore lint/correctness/noUnusedVariables: intentionally unused
+              const { children, className, node, ...rest } = props;
+              const match = /language-(\w+)/.exec(className || '');
+              return match ? (
+                <SyntaxHighlighter
+                  {...rest}
+                  children={String(children).replace(/\n$/, '')}
+                  style={nightOwl}
+                  language={match[1]}
+                  PreTag="div"
+                />
+              ) : (
+                <code {...rest} className={className}>
+                  {children}
+                </code>
+              );
+            },
+          }}
         />
       </div>
+      <div className="chat-footer">
+        {isLoading && !isUser && index === totalMessages ? (
+          <FontAwesomeIcon icon={faSpinner} spinPulse fixedWidth />
+        ) : null}
+        <span className="text-xs">
+          {isUser ? `${userMeta?.name ?? 'User '}` : 'Azure OpenAI GPT-4 '}
+        </span>
+      </div>
     </div>
-    <div
-      className={`prose relative chat-bubble${
-        isUser ? ' chat-bubble-primary' : ' bot'
-      }`}
-    >
-      {isUser
-        ? null
-        : (!isLoading || index !== totalMessages) && (
-            <CopyToClipboard textToCopy={markdownToText(message.content)} />
-          )}
-      <Markdown
-        children={message.content}
-        components={{
-          code(props) {
-            // biome-ignore lint/correctness/noUnusedVariables: intentionally unused
-            const { children, className, node, ...rest } = props;
-            const match = /language-(\w+)/.exec(className || '');
-            return match ? (
-              <SyntaxHighlighter
-                {...rest}
-                children={String(children).replace(/\n$/, '')}
-                style={nightOwl}
-                language={match[1]}
-                PreTag="div"
-              />
-            ) : (
-              <code {...rest} className={className}>
-                {children}
-              </code>
-            );
-          },
-        }}
-      />
-    </div>
-    <div className="chat-footer">
-      {isLoading && !isUser && index === totalMessages ? (
-        <FontAwesomeIcon icon={faSpinner} spinPulse fixedWidth />
-      ) : null}
-      <span className="text-xs">
-        {isUser ? `${userMeta?.name ?? 'User '}` : 'Azure OpenAI GPT-4 '}
-      </span>
-    </div>
-  </div>
-);
+  );
+};
 
 ChatBubble.displayName = 'ChatBubble';
 ChatBubble.propTypes = {
