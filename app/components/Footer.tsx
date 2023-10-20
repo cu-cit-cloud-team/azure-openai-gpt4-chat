7ff764ec67360d353066b15a809074e430f12e4f
Michael Sprague <mikesprague@gmail.com>
2023-10-20 07:10:00 -0400

    refactor(Footer): show correct modifier key for os

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Footer = ({
   formRef,
@@ -14,6 +14,16 @@ export const Footer = ({
       textAreaRef?.current?.focus();
     }
   }, [textAreaRef, systemMessageRef]);
+
+  const [isMac, setIsMac] = useState(false);
+  useEffect(() => {
+    setIsMac(navigator?.userAgent?.toLowerCase().includes('mac'));
+  }, []);
+
+  const [modifierKey, setModifierKey] = useState('⌘');
+  useEffect(() => {
+    setModifierKey(isMac ? '⌘' : 'Ctrl');
+  }, [isMac]);
 
   return (
     <footer className="fixed bottom-0 z-40 w-full px-4 py-8 text-center bg-base-200">
@@ -35,9 +45,10 @@ export const Footer = ({
         </button>
         <br />
         <small className="hidden bottom-8 lg:inline-block">
-          <kbd className="kbd">⌘</kbd>+<kbd className="kbd">Enter</kbd> to send
-          /<kbd className="kbd">⌘</kbd>+<kbd className="kbd">Esc</kbd> to clear
-          history
+          <kbd className="kbd">{modifierKey}</kbd>+
+          <kbd className="kbd">Enter</kbd> to send /
+          <kbd className="kbd">{modifierKey}</kbd>+
+          <kbd className="kbd">Esc</kbd> to clear history
         </small>
       </form>
     </footer>
