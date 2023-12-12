31e35e6805623493680b425eb8daf138f4d4644b
Michael Sprague <mikesprague@gmail.com>
2023-12-11 19:38:14 -0500

    style(stylelint): remove deprecated rule from config

--- stylelint.config.js
+++ stylelint.config.js
@@ -13,7 +13,6 @@ module.exports = {
         ],
       },
     ],
-    'declaration-block-trailing-semicolon': null,
     'no-descending-specificity': null,
     'no-empty-source': null,
     'at-rule-no-unknown': null,
