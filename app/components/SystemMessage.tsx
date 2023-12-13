dd8b8cc7ddd0b4e3519e31916c504d80128107f1
Michael Sprague <mikesprague@gmail.com>
2023-12-13 09:18:17 -0500

    style(SystemMessage): reduce width on small screens

--- app/components/SystemMessage.tsx
+++ app/components/SystemMessage.tsx
@@ -68,7 +68,7 @@ export const SystemMessage = ({
         display={'systemMessage'}
       />
       <textarea
-        className="w-64 h-48 m-2 whitespace-pre-line lg:w-96"
+        className="h-48 m-2 whitespace-pre-line w-52 lg:w-96"
         ref={systemMessageRef}
         onChange={(e) => setLocalSystemMessage(e.target.value)}
         value={localSystemMessage}
