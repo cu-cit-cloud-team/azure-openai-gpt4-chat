2a9d04ab3f0485f537ef1c83cf3f2a64ab580477
Michael Sprague <mikesprague@gmail.com>
2024-03-18 11:43:10 -0400

    feat(hooks/useDebounce): add hook for debouncing values

---
+++ app/hooks/useDebounce.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+export const useDebounce = (value, milliSeconds) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, milliSeconds);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, milliSeconds]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
