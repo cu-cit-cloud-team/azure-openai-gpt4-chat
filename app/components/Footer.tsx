9e236d8881b81f7df13896df473e4916e7c0ab98
Michael Sprague <mikesprague@gmail.com>
2024-03-18 13:32:17 -0400

    style(Footer): remove unused import

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { TokenCount } from './TokenCount.tsx';
 
