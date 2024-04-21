c15633881e274d625863b17410118b156144f0c9
Michael Sprague <mikesprague@gmail.com>
2024-04-21 15:38:15 -0400

    feat(TokenContext): add context and move token count logic into it

---
+++ app/contexts/TokenContext.tsx
@@ -0,0 +1,104 @@
+'use client';
+
+import propTypes from 'prop-types';
+import { createContext, useContext, useEffect, useState } from 'react';
+import type React from 'react';
+import useLocalStorageState from 'use-local-storage-state';
+
+export const TokenStateContext = createContext(null);
+export const TokenUpdaterContext = createContext(null);
+
+export const useTokenStateContext = () => {
+  const context = useContext(TokenStateContext);
+  if (context === null) {
+    throw new Error(
+      'useTokenStateContext must be used within a TokenStateProvider'
+    );
+  }
+  return context;
+};
+
+export const useTokenUpdaterContext = () => {
+  const context = useContext(TokenUpdaterContext);
+  if (context === null) {
+    throw new Error(
+      'useTokenUpdaterContext must be used within a TokenUpdaterProvider'
+    );
+  }
+  return context;
+};
+
+export const TokenStateProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const systemMessageMaxTokens = 400;
+  const [inputTokens, setInputTokens] = useState(0);
+  const [systemMessageTokens, setSystemMessageTokens] = useState(0);
+
+  const maxTokens = 16384;
+  const [remainingTokens, setRemainingTokens] = useState(16384);
+  const [remainingSystemTokens, setRemainingSystemTokens] = useState(
+    systemMessageMaxTokens
+  );
+
+  const [tokens, setTokens] = useLocalStorageState('tokens', {
+    defaultValue: {
+      input: inputTokens,
+      maximum: maxTokens,
+      remaining: remainingTokens,
+      systemMessage: systemMessageTokens,
+      systemMessageRemaining: remainingSystemTokens,
+    },
+  });
+
+  // set token counts
+  useEffect(() => {
+    setTokens({
+      input: inputTokens,
+      maximum: maxTokens,
+      remaining: remainingTokens,
+      systemMessage: systemMessageTokens,
+      systemMessageRemaining: remainingSystemTokens,
+    });
+  }, [
+    inputTokens,
+    remainingSystemTokens,
+    remainingTokens,
+    setTokens,
+    systemMessageTokens,
+  ]);
+
+  return (
+    <TokenStateContext.Provider
+      value={{
+        inputTokens,
+        maxTokens,
+        remainingSystemTokens,
+        remainingTokens,
+        systemMessageMaxTokens,
+        tokens,
+      }}
+    >
+      <TokenUpdaterContext.Provider
+        value={{
+          setInputTokens,
+          setRemainingSystemTokens,
+          setRemainingTokens,
+          setSystemMessageTokens,
+          setTokens,
+        }}
+      >
+        {children}
+      </TokenUpdaterContext.Provider>
+    </TokenStateContext.Provider>
+  );
+};
+
+TokenStateContext.propTypes = {
+  children: propTypes.node,
+};
+TokenStateContext.displayName = 'TokenStateContext';
+
+export default TokenStateContext;
