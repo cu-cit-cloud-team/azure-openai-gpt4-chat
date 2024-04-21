e6a220b5694b194fbb50fbc61d29a479b6262d3a
Michael Sprague <mikesprague@gmail.com>
2024-04-21 15:47:00 -0400

    refactor(TokenCount): use new context/provider

--- app/components/TokenCount.tsx
+++ app/components/TokenCount.tsx
@@ -1,94 +1,80 @@
 import PropTypes from 'prop-types';
-import { memo, useEffect, useMemo, useState } from 'react';
-import useLocalStorageState from 'use-local-storage-state';
+import { memo, useEffect, useMemo } from 'react';
 
 import { useDebounce } from '@/app/hooks/useDebounce';
 
+import {
+  useTokenStateContext,
+  useTokenUpdaterContext,
+} from '../contexts/TokenContext';
+
 import { getTokenCount } from '@/app/utils/tokens';
 
-export const TokenCount = memo(
-  ({ input = '', systemMessage, display = 'input' }) => {
-    const systemMessageMaxTokens = 400;
-    const [inputTokens, setInputTokens] = useState(0);
-    const [systemMessageTokens, setSystemMessageTokens] = useState(0);
+export const TokenCount = ({
+  input = '',
+  systemMessage,
+  display = 'input',
+}) => {
+  const { maxTokens, systemMessageMaxTokens, tokens } = useTokenStateContext();
+  const {
+    setInputTokens,
+    setRemainingSystemTokens,
+    setRemainingTokens,
+    setSystemMessageTokens,
+  } = useTokenUpdaterContext();
 
-    const tokenInput = useDebounce(input, 200);
+  const tokenInput = useDebounce(input, 200);
 
-    const maxTokens = 16384;
-    const [remainingTokens, setRemainingTokens] = useState(16384);
-    const [remainingSystemTokens, setRemainingSystemTokens] = useState(
-      systemMessageMaxTokens
+  const updateSystemMessageCount = useMemo(() => {
+    return getTokenCount(systemMessage);
+  }, [systemMessage]);
+
+  const updateInputCount = useMemo(() => {
+    return getTokenCount(tokenInput);
+  }, [tokenInput]);
+
+  // update token counts
+  useEffect(() => {
+    setSystemMessageTokens(updateSystemMessageCount);
+    setInputTokens(updateInputCount);
+    setRemainingTokens(
+      maxTokens - (updateSystemMessageCount + updateInputCount)
     );
+    setRemainingSystemTokens(systemMessageMaxTokens - updateSystemMessageCount);
+  }, [
+    maxTokens,
+    setInputTokens,
+    setRemainingSystemTokens,
+    setRemainingTokens,
+    setSystemMessageTokens,
+    systemMessageMaxTokens,
+    updateInputCount,
+    updateSystemMessageCount,
+  ]);
 
-    const [tokens, setTokens] = useLocalStorageState('tokens', {
-      defaultValue: {
-        input: inputTokens,
-        maximum: maxTokens,
-        remaining: remainingTokens,
-        systemMessage: systemMessageTokens,
-        systemMessageRemaining: remainingSystemTokens,
-      },
-    });
-
-    const updateSystemMessageCount = useMemo(() => {
-      return getTokenCount(systemMessage);
-    }, [systemMessage]);
-
-    const updateInputCount = useMemo(() => {
-      return getTokenCount(tokenInput);
-    }, [tokenInput]);
-
-    // update token counts
-    useEffect(() => {
-      const systemMessageCount = updateSystemMessageCount;
-      setSystemMessageTokens(systemMessageCount);
-      const inputCount = updateInputCount;
-      setInputTokens(inputCount);
-      setRemainingTokens(maxTokens - (systemMessageCount + inputCount));
-      setRemainingSystemTokens(systemMessageMaxTokens - systemMessageCount);
-    }, [updateInputCount, updateSystemMessageCount]);
-
-    // set token counts
-    useEffect(() => {
-      setTokens({
-        input: inputTokens,
-        maximum: maxTokens,
-        remaining: remainingTokens,
-        systemMessage: systemMessageTokens,
-        systemMessageRemaining: remainingSystemTokens,
-      });
-    }, [
-      inputTokens,
-      remainingSystemTokens,
-      remainingTokens,
-      setTokens,
-      systemMessageTokens,
-    ]);
-
-    return (
-      <>
-        <div
-          className={`${
-            display === 'systemMessage' ? '-mb-3' : 'mb-1'
-          } text-xs text-base-content opacity-50 uppercase cursor-default`}
-          key={`${display}-token-count`}
-        >
-          <strong>
-            {tokens[display]}{' '}
-            <span className="font-normal">
-              Token{tokens.input === 1 ? '' : 's '}
-            </span>{' '}
-            /{' '}
-            {display === 'systemMessage'
-              ? tokens.systemMessageRemaining
-              : tokens.remaining}{' '}
-            <span className="font-normal">Remaining</span>
-          </strong>
-        </div>
-      </>
-    );
-  }
-);
+  return (
+    <>
+      <div
+        className={`${
+          display === 'systemMessage' ? '-mb-3' : 'mb-1'
+        } text-xs text-base-content opacity-50 uppercase cursor-default`}
+        key={`${display}-token-count`}
+      >
+        <strong>
+          {tokens[display]}{' '}
+          <span className="font-normal">
+            Token{tokens.input === 1 ? '' : 's '}
+          </span>{' '}
+          /{' '}
+          {display === 'systemMessage'
+            ? tokens.systemMessageRemaining
+            : tokens.remaining}{' '}
+          <span className="font-normal">Remaining</span>
+        </strong>
+      </div>
+    </>
+  );
+};
 
 TokenCount.displayName = 'TokenCount';
 
