f66473e12cf9d95b738c8f4c18584361a7ea644c
Michael Sprague <mikesprague@gmail.com>
2024-04-22 19:26:59 -0400

    refactor(App/DefaultsContext): move some vars/logic into context

---
+++ app/contexts/DefaultsContext.tsx
@@ -0,0 +1,141 @@
+'use client';
+
+import propTypes from 'prop-types';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+} from 'react';
+import useLocalStorageState from 'use-local-storage-state';
+
+import { getItem, removeItem } from '@/app/utils/localStorage';
+
+import { database } from '@/app/database/database.config';
+
+export const DefaultsContext = createContext(null);
+export const DefaultsUpdaterContext = createContext(null);
+
+export const useDefaultsContext = () => {
+  const context = useContext(DefaultsContext);
+  if (context === null) {
+    throw new Error(
+      'useDefaultsContext must be used within a DefaultsProvider'
+    );
+  }
+  return context;
+};
+
+export const useDefaultsUpdaterContext = () => {
+  const context = useContext(DefaultsUpdaterContext);
+  if (context === null) {
+    throw new Error(
+      'useDefaultsUpdaterContext must be used within a DefaultsProvider'
+    );
+  }
+  return context;
+};
+
+export const DefaultsProvider = ({ children }) => {
+  // migrate localStorage to indexedDB
+  useEffect(() => {
+    const migrateLocalStorage = async () => {
+      try {
+        const messages = getItem('messages');
+        if (messages) {
+          await database.transaction('rw', database.messages, async () => {
+            for await (const message of messages) {
+              await database.messages.put(message);
+            }
+            removeItem('messages');
+          });
+        }
+        return;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    migrateLocalStorage();
+  }, []);
+
+  const [systemMessage, setSystemMessage] = useLocalStorageState(
+    'systemMessage',
+    {
+      defaultValue: 'You are a helpful AI assistant.',
+    }
+  );
+
+  const [parameters, setParameters] = useLocalStorageState('parameters', {
+    defaultValue: {
+      model: 'gpt-4',
+      temperature: '1',
+      top_p: '1',
+      frequency_penalty: '0',
+      presence_penalty: '0',
+    },
+  });
+
+  const maxTokens = useMemo(() => {
+    return parameters?.model === 'gpt-4' ? 128000 : 16384;
+  }, [parameters]);
+
+  const systemMessageMaxTokens = 400;
+
+  const clearHistory = useCallback(async (doConfirm = true) => {
+    const clearMessages = async () => {
+      try {
+        await database.messages.clear();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (!doConfirm) {
+      await clearMessages();
+      window.location.reload();
+    } else if (confirm('Are you sure you want to clear the chat history?')) {
+      await clearMessages();
+      window.location.reload();
+    }
+  }, []);
+
+  const handleChatError = useCallback((error) => {
+    console.error(error);
+    throw error;
+  }, []);
+
+  const addMessage = useCallback(async (message) => {
+    await database.messages.put(message);
+  }, []);
+
+  return (
+    <DefaultsContext.Provider
+      value={{
+        maxTokens,
+        parameters,
+        systemMessage,
+        systemMessageMaxTokens,
+      }}
+    >
+      <DefaultsUpdaterContext.Provider
+        value={{
+          addMessage,
+          clearHistory,
+          handleChatError,
+          setParameters,
+          setSystemMessage,
+        }}
+      >
+        {children}
+      </DefaultsUpdaterContext.Provider>
+    </DefaultsContext.Provider>
+  );
+};
+
+DefaultsProvider.displayName = 'DefaultsProvider';
+DefaultsProvider.propTypes = {
+  children: propTypes.node.isRequired,
+};
+
+export default DefaultsProvider;
--- app/page.tsx
+++ app/page.tsx
@@ -4,67 +4,32 @@ import { useChat } from 'ai/react';
 import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
 import { useLiveQuery } from 'dexie-react-hooks';
-import { useCallback, useEffect, useState } from 'react';
+import { memo, useEffect } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
-import useLocalStorageState from 'use-local-storage-state';
 
 import { Footer } from '@/app/components/Footer';
 import { Header } from '@/app/components/Header';
 import { Messages } from '@/app/components/Messages';
 
+import {
+  useDefaultsContext,
+  useDefaultsUpdaterContext,
+} from '@/app/contexts/DefaultsContext';
 import { useRefsContext } from '@/app/contexts/RefsContext';
 import { useUserMetaContext } from '@/app/contexts/UserMetaContext';
-
-import { getItem, removeItem } from '@/app/utils/localStorage';
 
 import { database } from '@/app/database/database.config';
 
 dayjs.extend(timezone);
 
 export const App = () => {
+  const { parameters, systemMessage } = useDefaultsContext();
+  const { addMessage, clearHistory, handleChatError, setSystemMessage } =
+    useDefaultsUpdaterContext();
   const { userMeta } = useUserMetaContext();
   const { formRef, textAreaRef } = useRefsContext();
 
-  // migrate localStorage to indexedDB
-  useEffect(() => {
-    const migrateLocalStorage = async () => {
-      try {
-        const messages = getItem('messages');
-        if (messages) {
-          await database.transaction('rw', database.messages, async () => {
-            for await (const message of messages) {
-              await database.messages.put(message);
-            }
-            removeItem('messages');
-          });
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    migrateLocalStorage();
-  }, []);
-
-  const [systemMessage, setSystemMessage] = useLocalStorageState(
-    'systemMessage',
-    {
-      defaultValue: 'You are a helpful AI assistant.',
-    }
-  );
-
-  const [parameters] = useLocalStorageState('parameters', {
-    defaultValue: {
-      model: 'gpt-4',
-      temperature: '1',
-      top_p: '1',
-      frequency_penalty: '0',
-      presence_penalty: '0',
-    },
-  });
-
-  const [savedMessages, setSavedMessages] = useState([]);
-
-  const dbMessages = useLiveQuery(async () => {
+  const savedMessages = useLiveQuery(async () => {
     let messages = await database.messages.toArray();
     messages = messages.sort(
       (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
@@ -72,23 +37,7 @@ export const App = () => {
     return messages;
   }, [database.messages]);
 
-  useEffect(() => {
-    if (dbMessages?.length) {
-      setSavedMessages(dbMessages);
-    }
-  }, [dbMessages]);
-
-  const handleChatError = useCallback((error) => {
-    console.error(error);
-    throw error;
-  }, []);
-
-  const addMessage = useCallback(async (message) => {
-    await database.messages.put(message);
-  }, []);
-
   const {
-    error,
     handleInputChange,
     handleSubmit,
     input,
@@ -116,7 +65,7 @@ export const App = () => {
 
   // update indexedDB when messages changes
   useEffect(() => {
-    if (messages?.length !== savedMessages?.length) {
+    if (savedMessages && savedMessages?.length !== messages?.length) {
       if (messages[messages.length - 1].role === 'user' || !isLoading) {
         addMessage(messages[messages.length - 1]);
       }
@@ -146,24 +95,6 @@ export const App = () => {
     return () => {
       window.removeEventListener('storage', handleStorageChanges);
     };
-  }, []);
-
-  const clearHistory = useCallback(async (doConfirm = true) => {
-    const clearMessages = async () => {
-      try {
-        await database.messages.clear();
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    if (!doConfirm) {
-      await clearMessages();
-      window.location.reload();
-    } else if (confirm('Are you sure you want to clear the chat history?')) {
-      await clearMessages();
-      window.location.reload();
-    }
   }, []);
 
   const textareaElement = textAreaRef.current;
@@ -205,7 +136,7 @@ export const App = () => {
     };
   }, [formRef, textareaElement, clearHistory]);
 
-  const ErrorFallback = ({ error, resetErrorBoundary }) => {
+  const ErrorFallback = memo(({ error, resetErrorBoundary }) => {
     return (
       <dialog className="modal modal-bottom sm:modal-middle errorModal">
         <div className="w-11/12 max-w-5xl modal-box bg-error-content">
@@ -216,7 +147,7 @@ export const App = () => {
               type="button"
               className="btn btnReload text-error-content btn-error"
               onClick={() => {
-                // resetErrorBoundary();
+                resetErrorBoundary();
                 window.location.reload();
               }}
             >
@@ -226,7 +157,7 @@ export const App = () => {
         </div>
       </dialog>
     );
-  };
+  });
 
   return (
     <>
@@ -236,13 +167,10 @@ export const App = () => {
           systemMessage={systemMessage}
           setSystemMessage={setSystemMessage}
           input={input}
-          clearHistory={clearHistory}
         />
         <Messages
           isLoading={isLoading}
           messages={messages}
-          savedMessages={savedMessages}
-          error={error}
           reload={reload}
           stop={stop}
         />
