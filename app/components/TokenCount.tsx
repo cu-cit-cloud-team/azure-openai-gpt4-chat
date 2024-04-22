b89f8252c0c79c4a1d8c28de5a0289916d4cd778
Michael Sprague <mikesprague@gmail.com>
2024-04-22 14:45:32 -0400

    perf(TokenCount): memoize component

--- app/components/TokenCount.tsx
+++ app/components/TokenCount.tsx
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
-import { useEffect, useMemo } from 'react';
-
-import { useDebounce } from '@/app/hooks/useDebounce';
+import { memo, useEffect, useMemo } from 'react';
 
 import {
   useTokenStateContext,
@@ -10,71 +8,70 @@ import {
 
 import { getTokenCount } from '@/app/utils/tokens';
 
-export const TokenCount = ({
-  input = '',
-  systemMessage,
-  display = 'input',
-}) => {
-  const { maxTokens, systemMessageMaxTokens, tokens } = useTokenStateContext();
-  const {
-    setInputTokens,
-    setRemainingSystemTokens,
-    setRemainingTokens,
-    setSystemMessageTokens,
-  } = useTokenUpdaterContext();
+export const TokenCount = memo(
+  ({ input = '', systemMessage, display = 'input' }) => {
+    const { maxTokens, systemMessageMaxTokens, tokens } =
+      useTokenStateContext();
+    const {
+      setInputTokens,
+      setRemainingSystemTokens,
+      setRemainingTokens,
+      setSystemMessageTokens,
+    } = useTokenUpdaterContext();
 
-  const tokenInput = useDebounce(input, 200);
+    const updateSystemMessageCount = useMemo(() => {
+      return getTokenCount(systemMessage);
+    }, [systemMessage]);
 
-  const updateSystemMessageCount = useMemo(() => {
-    return getTokenCount(systemMessage);
-  }, [systemMessage]);
+    const updateInputCount = useMemo(() => {
+      return getTokenCount(input);
+    }, [input]);
 
-  const updateInputCount = useMemo(() => {
-    return getTokenCount(tokenInput);
-  }, [tokenInput]);
+    // update token counts
+    useEffect(() => {
+      setSystemMessageTokens(updateSystemMessageCount);
+      setInputTokens(updateInputCount);
+      setRemainingTokens(
+        maxTokens - (updateSystemMessageCount + updateInputCount)
+      );
+      setRemainingSystemTokens(
+        systemMessageMaxTokens - updateSystemMessageCount
+      );
+    }, [
+      maxTokens,
+      setInputTokens,
+      setRemainingSystemTokens,
+      setRemainingTokens,
+      setSystemMessageTokens,
+      systemMessageMaxTokens,
+      updateInputCount,
+      updateSystemMessageCount,
+    ]);
 
-  // update token counts
-  useEffect(() => {
-    setSystemMessageTokens(updateSystemMessageCount);
-    setInputTokens(updateInputCount);
-    setRemainingTokens(
-      maxTokens - (updateSystemMessageCount + updateInputCount)
+    return (
+      <>
+        <div
+          className={`${
+            display === 'systemMessage' ? '-mb-3' : 'mb-1'
+          } text-xs text-base-content opacity-50 uppercase cursor-default`}
+          key={`${display}-token-count`}
+        >
+          <strong>
+            {tokens[display]}{' '}
+            <span className="font-normal">
+              Token{tokens.input === 1 ? '' : 's '}
+            </span>{' '}
+            /{' '}
+            {display === 'systemMessage'
+              ? tokens.systemMessageRemaining
+              : tokens.remaining}{' '}
+            <span className="font-normal">Remaining</span>
+          </strong>
+        </div>
+      </>
     );
-    setRemainingSystemTokens(systemMessageMaxTokens - updateSystemMessageCount);
-  }, [
-    maxTokens,
-    setInputTokens,
-    setRemainingSystemTokens,
-    setRemainingTokens,
-    setSystemMessageTokens,
-    systemMessageMaxTokens,
-    updateInputCount,
-    updateSystemMessageCount,
-  ]);
-
-  return (
-    <>
-      <div
-        className={`${
-          display === 'systemMessage' ? '-mb-3' : 'mb-1'
-        } text-xs text-base-content opacity-50 uppercase cursor-default`}
-        key={`${display}-token-count`}
-      >
-        <strong>
-          {tokens[display]}{' '}
-          <span className="font-normal">
-            Token{tokens.input === 1 ? '' : 's '}
-          </span>{' '}
-          /{' '}
-          {display === 'systemMessage'
-            ? tokens.systemMessageRemaining
-            : tokens.remaining}{' '}
-          <span className="font-normal">Remaining</span>
-        </strong>
-      </div>
-    </>
-  );
-};
+  }
+);
 
 TokenCount.displayName = 'TokenCount';
 
