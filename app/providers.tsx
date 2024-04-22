2afeaf86d5cddceb8fe22a198678ae9f48b1b5eb
Michael Sprague <mikesprague@gmail.com>
2024-04-22 16:03:43 -0400

    refactor(Providers): add RefsProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,14 @@
 import type React from 'react';
 
+import { RefsProvider } from '@/app/contexts/RefsContext';
 import { UserMetaProvider } from '@/app/contexts/UserMetaContext';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <UserMetaProvider>{children}</UserMetaProvider>
+      <RefsProvider>
+        <UserMetaProvider>{children}</UserMetaProvider>
+      </RefsProvider>
     </>
   );
 };
