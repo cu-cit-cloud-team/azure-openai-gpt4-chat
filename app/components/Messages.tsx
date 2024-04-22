80208bc6b7741c8af11e74f2c6c32e5d6ed262ae
Michael Sprague <mikesprague@gmail.com>
2024-04-22 19:33:33 -0400

    refactor(Messages): clean up props/propTypes

--- app/components/Messages.tsx
+++ app/components/Messages.tsx
@@ -4,50 +4,42 @@ import { memo, useEffect } from 'react';
 
 import { ChatBubble } from '@/app/components/ChatBubble';
 
-export const Messages = memo(
-  ({ isLoading, messages, savedMessages, error, reload, stop }) => {
-    // biome-ignore lint/correctness/useExhaustiveDependencies: scroll to bottom fix
-    useEffect(() => {
-      window.scrollTo({
-        left: 0,
-        top: document.body.scrollHeight,
-        behavior: 'smooth',
-      });
-    }, [messages]);
+export const Messages = memo(({ isLoading, messages, reload, stop }) => {
+  // biome-ignore lint/correctness/useExhaustiveDependencies: scroll to bottom fix
+  useEffect(() => {
+    window.scrollTo({
+      left: 0,
+      top: document.body.scrollHeight,
+      behavior: 'smooth',
+    });
+  }, [messages]);
 
-    return (
-      <div className="z-0 overflow-auto bg-base-100">
-        <div className="flex flex-col w-full h-full max-w-6xl min-h-screen pt-64 mx-auto mb-12 pb-28">
-          {messages.length > 0
-            ? messages.map((m, idx) => {
-                return (
-                  <ChatBubble
-                    key={nanoid()}
-                    message={m}
-                    index={idx}
-                    isUser={m.role === 'user'}
-                    isLoading={isLoading}
-                    totalMessages={messages.length - 1}
-                    savedMessages={savedMessages}
-                    stop={stop}
-                    reload={reload}
-                    error={error}
-                  />
-                );
-              })
-            : null}
-        </div>
+  return (
+    <div className="z-0 overflow-auto bg-base-100">
+      <div className="flex flex-col w-full h-full max-w-6xl min-h-screen pt-64 mx-auto mb-12 pb-28">
+        {messages.length > 0
+          ? messages.map((m, idx) => {
+              return (
+                <ChatBubble
+                  key={nanoid()}
+                  index={idx}
+                  isLoading={isLoading}
+                  isUser={m.role === 'user'}
+                  message={m}
+                  reload={reload}
+                  stop={stop}
+                  totalMessages={messages.length - 1}
+                />
+              );
+            })
+          : null}
       </div>
-    );
-  }
-);
+    </div>
+  );
+});
 
 Messages.displayName = 'Messages';
 Messages.propTypes = {
-  error: PropTypes.oneOfType([
-    PropTypes.instanceOf(Error),
-    PropTypes.instanceOf(undefined),
-  ]),
   isLoading: PropTypes.bool.isRequired,
   messages: PropTypes.arrayOf(PropTypes.object).isRequired,
   reload: PropTypes.func.isRequired,
@@ -57,4 +49,5 @@ Messages.propTypes = {
   ]).isRequired,
   stop: PropTypes.func.isRequired,
 };
+
 export default Messages;
