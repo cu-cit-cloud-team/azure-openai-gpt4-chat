3cb84fe5d3f1fc2157cee315f2f8996977fdd2e0
Michael Sprague <mikesprague@gmail.com>
2024-04-29 09:22:56 -0400

    refactor(RootLayout): add attribute for next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <div className="page-wrapper">
           <Providers>{children}</Providers>
