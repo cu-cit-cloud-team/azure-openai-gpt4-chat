c09aaa9cd62e432adc762e716c96835061a1e683
Michael Sprague <mikesprague@gmail.com>
2024-05-06 12:21:36 -0400

    refactor(utils/tokens): use gpt-4-turbo for token model

--- app/utils/tokens.ts
+++ app/utils/tokens.ts
@@ -1,6 +1,6 @@
 import { encodingForModel } from 'js-tiktoken';
 
-const tokenizer = encodingForModel('gpt-4-0125-preview');
+const tokenizer = encodingForModel('gpt-4-turbo');
 
 export const getTokenCount = (string): number => {
   const tokenCount = tokenizer.encode(string).length;
