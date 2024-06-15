03fa7a1b0a075c690527beffdd7fd64c6342daef
Michael Sprague <mikesprague@gmail.com>
2024-06-15 18:00:12 -0400

    refactor(utils/markdownToText): update for marked v13

--- app/utils/markdownToText.ts
+++ app/utils/markdownToText.ts
@@ -1,10 +1,22 @@
 import { decode, encode } from 'he';
 import { marked } from 'marked';
 
-const block = (text: string) => `${text}\n\n`;
-const escapeBlock = (text: string) => `${encode(text)}\n\n`;
-const line = (text: string) => `${text}\n`;
-const inline = (text: string) => text;
+const block = (token) => {
+  const text = marked.parseInline(token.text || token);
+  return `${text}\n\n`;
+};
+const escapeBlock = (token) => {
+  const text = marked.parseInline(token.text);
+  return `${encode(text)}\n\n`;
+};
+const line = (token) => {
+  const text = marked.parseInline(token.text);
+  return `${text}\n`;
+};
+const inline = (token) => {
+  const text = marked.parseInline(token.text);
+  return text;
+};
 const newline = () => '\n';
 const empty = () => '';
 
@@ -15,13 +27,23 @@ const TextRenderer: marked.Renderer = {
   html: empty,
   heading: block,
   hr: newline,
-  list: (text) => block(text.trim()),
+  list: (token) => {
+    const text = marked.parseInline(token.raw);
+    return block(text.trim());
+  },
   listitem: line,
   checkbox: empty,
   paragraph: block,
+  space: empty,
   table: (header, body) => line(header + body),
-  tablerow: (text) => line(text.trim()),
-  tablecell: (text) => `${text} `,
+  tablerow: (token) => {
+    const text = marked.parseInline(token.text);
+    return line(text.trim());
+  },
+  tablecell: (token) => {
+    const text = marked.parseInline(token.text);
+    return `${text} `;
+  },
   // inline elements
   strong: inline,
   bold: inline,
@@ -30,8 +52,14 @@ const TextRenderer: marked.Renderer = {
   codespan: inline,
   br: newline,
   del: inline,
-  link: (_0, _1, text) => text,
-  image: (_0, _1, text) => text,
+  link: (_0, _1, token) => {
+    const text = marked.parseInline(token.text);
+    return text;
+  },
+  image: (_0, _1, token) => {
+    const text = marked.parseInline(token.text);
+    return text;
+  },
   text: inline,
   // etc.
   options: {},
@@ -45,7 +73,11 @@ export const markdownToText = (
     return '';
   }
 
-  const unmarked = marked(markdown, { ...options, renderer: TextRenderer });
+  const unmarked = marked(markdown, {
+    ...options,
+    useNewRenderer: true,
+    renderer: TextRenderer,
+  });
   const unescaped = decode(unmarked);
   const trimmed = unescaped.trim();
   return trimmed;
