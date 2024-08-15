f30cbaf666cd7e3017d3b41ddbe7d30059bb527b
Michael Sprague <mikesprague@gmail.com>
2024-08-15 09:44:44 -0400

    refactor(utils/tokens): default to gpt-4o

--- app/utils/tokens.ts
+++ app/utils/tokens.ts
@@ -1,6 +1,6 @@
 import { encodingForModel } from 'js-tiktoken';
 
-const tokenizer = encodingForModel('gpt-4-turbo');
+const tokenizer = encodingForModel('gpt-4o');
 
 export const getTokenCount = (string): number => {
   const tokenCount = tokenizer.encode(string).length;
