14f6f0f6ab0c9495bfdf667b8c7d7497cd00570e
Michael Sprague <mikesprague@gmail.com>
2024-09-18 11:53:58 -0400

    feat(database): add columns to track model

--- app/database/database.config.ts
+++ app/database/database.config.ts
@@ -2,9 +2,23 @@ import Dexie from 'dexie';
 
 export const database = new Dexie('database');
 
+// initial version
 database.version(1).stores({
   messages: '&id, role, content, createdAt',
 });
+
+// add model information to each message
+database
+  .version(2)
+  .stores({
+    messages: '&id, role, content, createdAt, model, modelString',
+  })
+  .upgrade((tx) => {
+    return tx.messages.toCollection().modify((message) => {
+      message.model = 'gpt-4o';
+      message.modelString = 'Azure OpenAI GPT-4o (2024-08-06)';
+    });
+  });
 
 export const messagesTable = database.table('messages');
 
