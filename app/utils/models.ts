9e67a0c7036f80d99fa97ace243c6d55461de20e
Michael Sprague <mikesprague@gmail.com>
2025-01-30 10:51:29 -0500

    refactor(urils/models): update a couple model versions

--- app/utils/models.ts
+++ app/utils/models.ts
@@ -9,13 +9,13 @@ export interface Models {
 }
 
 export const models: Models = [
-  { name: 'gpt-35-turbo', displayName: 'GPT-3.5 Turbo (1106)' },
+  { name: 'gpt-35-turbo', displayName: 'GPT-3.5 Turbo (0125)' },
   { name: 'gpt-4', displayName: 'GPT-4 (1106)' },
   {
     name: 'gpt-4-turbo',
     displayName: 'GPT-4 Turbo (2024-04-09)',
   },
-  { name: 'gpt-4o', displayName: 'GPT-4o (2024-08-06)', default: true },
+  { name: 'gpt-4o', displayName: 'GPT-4o (2024-11-20)', default: true },
   {
     name: 'gpt-4o-mini',
     displayName: 'GPT-4o Mini (2024-07-18)',
