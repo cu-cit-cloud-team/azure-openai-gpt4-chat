127607c74ff4cb461fb2c07d779eac9bdcea9bd6
Michael Sprague <mikesprague@gmail.com>
2025-03-19 10:33:57 -0400

    style(layout): add suppressHydrationWarning to body tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body suppressHydrationWarning>
         <div className="page-wrapper">
           <Providers>{children}</Providers>
         </div>
