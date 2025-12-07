1eaf06661af02449a09fc22b823e83e00e45e324
Michael Sprague <mikesprague@gmail.com>
2025-12-07 18:44:24 -0500

    build(next): update `Permissions-Policy` header

--- next.config.mjs
+++ next.config.mjs
@@ -50,7 +50,7 @@ const nextConfig = {
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()',
+            value: 'camera=(), microphone=(self), geolocation=()',
           },
         ],
       },
