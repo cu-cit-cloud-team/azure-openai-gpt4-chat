92b42d82991094e00045741a192682aa87a699b1
Michael Sprague <mikesprague@gmail.com>
2026-01-20 09:52:44 -0500

    fix(ErrorFallback): update to use latest api

--- app/components/ErrorFallback.tsx
+++ app/components/ErrorFallback.tsx
@@ -1,21 +1,15 @@
+import { type FallbackProps, getErrorMessage } from 'react-error-boundary';
+
 import { Alert, AlertDescription } from '@/app/components/ui/alert';
 import { Button } from '@/app/components/ui/button';
 
-export interface ErrorFallbackProps {
-  error: Error;
-  resetErrorBoundary: () => void;
-}
-
-export const ErrorFallback = ({
-  error,
-  resetErrorBoundary,
-}: ErrorFallbackProps) => {
+export const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-background/80 backdrop-blur-sm">
       <Alert variant="destructive" className="w-11/12 max-w-lg">
         <AlertDescription className="space-y-4">
           <h3 className="text-lg font-bold">Error</h3>
-          <p>{error?.message}</p>
+          <p>{getErrorMessage(error)}</p>
           <Button
             variant="destructive"
             onClick={() => {
